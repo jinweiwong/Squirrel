107d3497431ab3de8339326f01e6195df68642e9
Jin Wei Wong <jinwei@berkeley.edu>
2022-02-19 08:10:14 -0800

    Initialized Git

---
+++ SquirrelIOS/SquirrelIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquirrelIOSApp.swift
+//  SquirrelIOS
+//
+//  Created by Wong Jin Wei on 19/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SquirrelIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
